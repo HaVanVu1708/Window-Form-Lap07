8c39910ac84c699ef98c5db19b9d58c39b4a0f3e
ha van vu <62437220+HaVanVu1708@users.noreply.github.com>
2020-10-22 09:47:22 +0700

    lap07

---
+++ FakeNews/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using FakeNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FakeNews
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<NewsItem> NewsItems;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NewsItems = new ObservableCollection<NewsItem>();
+        }
+        
+        private void LixBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(Financial.IsSelected)
+            {
+                NewsManager.GetNews("Financial", NewsItems);
+                TitleTextBlock.Text = "Food";
+            }
+            else if (Food.IsSelected)
+            {
+                NewsManager.GetNews("Food", NewsItems);
+                TitleTextBlock.Text = "Food";
+            }
+        }
+
+    }
+}
---
+++ ObservableCollection2/Models/Icon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollection2.Models
+{
+   public class Icon
+    {
+        public string IconPath { get; set; }
+    }
+}
